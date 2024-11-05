9ef61e0dbcd29d102a20168c2a972a35bbeb11ae
Josh <joshua.benza@student.tamdistrict.org>
2024-11-05 13:55:13 -0800

    update tuesday 11/5

--- mason.js
+++ mason.js
@@ -2,5 +2,5 @@ let masonEl = document.getElementById("mason-count")
 let count = 0
 function add(){
 count += 1
-masonEl.innerText=count
+masonEl.innerText="you have clicked mason " + count + " times"
 }
