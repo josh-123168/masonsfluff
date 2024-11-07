79e3f48b5966906bd95f29f4cab5b6e9cae9d6e4
Josh <joshua.benza@student.tamdistrict.org>
2024-11-07 12:55:09 -0800

    new buttons

--- mason.js
+++ mason.js
@@ -1,6 +1,7 @@
 let masonEl = document.getElementById("mason-count")
 let count = 0
-function add(){
-count += 1
-masonEl.innerText="you have clicked mason " + count + " times"
+
+function add() {
+    count += 1
+    masonEl.innerText="you have clicked mason " + count + " times"
 }
