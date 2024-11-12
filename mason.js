0fa7580ef0751daefd756cf386fc6cb6bb6e0278
Josh <joshua.benza@student.tamdistrict.org>
2024-11-12 13:54:14 -0800

    ADD MASON QUIZ
    Co-authored-by: adamscheer4 <adamscheer4@users.noreply.github.com>
    Co-authored-by: ethanfinkelstein28 <ethanfinkelstein28@users.noreply.github.com>
    Co-authored-by: EmilioMancl <EmilioMancl@users.noreply.github.com>

--- mason.js
+++ mason.js
@@ -3,5 +3,5 @@ let count = 0
 
 function add() {
     count += 1
-    masonEl.innerText="you have clicked mason " + count + " times"
+    masonEl.innerText="You have touched Mason😩! " + count + " times"
 }
