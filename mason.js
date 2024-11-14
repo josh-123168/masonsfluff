f8137ff30984721fa75da817a88c8d612ae2e91c
Josh <joshua.benza@student.tamdistrict.org>
2024-11-14 12:33:58 -0800

    click msg

--- mason.js
+++ mason.js
@@ -3,5 +3,5 @@ let count = 0
 
 function add() {
     count += 1
-    masonEl.innerText="You have touched Mason😩! " + count + " times"
+    masonEl.innerText="You have touched Mason " + count + " times!"
 }
