635b1c85a3c2d72834ab3885defff084e90d9f82
Josh <joshua.benza@student.tamdistrict.org>
2024-12-03 13:54:55 -0800

    mason matcher and roshambo

--- pages/mason-row-sham-bow/mason.js
+++ pages/mason-row-sham-bow/mason.js
@@ -1,25 +1,53 @@
-let move=0
+let move = 0
 let testEl = document.getElementById("test")
+let masonEl = document.getElementById("mason")
 let masonMove = 0
 function rock() {
-    move=1
-    testEl.innerText = move
- }
- function paper() {
-    move=2
-    testEl.innerText = move
- }
- function scissors() {
-    move=3
-    testEl.innerText = move
- }
- function play() {
+    move = 1
+    testEl.innerText = "Rock"
+    mason.innerText = ""
+}
+function paper() {
+    move = 2
+    testEl.innerText = "Paper"
+    mason.innerText = ""
+}
+function scissors() {
+    move = 3
+    testEl.innerText = "Scissors"
+    mason.innerText = ""
+}
+function play() {
 
-      masonMove = Math.floor(Math.random()*3 + 1)
-      testEl.innerText = masonMove
+    const ROCK = 1
 
-      if(masonMove=move){
+    masonMove = Math.floor(Math.random() * 3 + 1)
+
+
+    if (masonMove == ROCK) { mason.innerText = "I choose rock!" }
+    if (masonMove == 2) { mason.innerText = "I choose paper!" }
+    if (masonMove == 3) { mason.innerText = "I choose scissors!" }
+
+    if (masonMove == move) {
         testEl.innerText = "Tie!"
-      }
-      if(masonMove=)
     }
+    if (masonMove == 1 & move == 2) {
+        testEl.innerText = "you win this time >:[]"
+    }
+    if (masonMove == 1 & move == 3) {
+        testEl.innerText = "I win, hahahaha"
+    }
+    if (masonMove == 2 & move == 1) {
+        testEl.innerText = "I win, hahahaha"
+    }
+    if (masonMove == 2 & move == 3) {
+        testEl.innerText = "you win this time >:[]"
+    }
+    if (masonMove == 3 & move == 1) {
+        testEl.innerText = "you win this time >:[]"
+    }
+    if (masonMove == 3 & move == 2) {
+        testEl.innerText = "I win, hahahaha"
+    }
+
+}
